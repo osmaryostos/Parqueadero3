b2ac33507f6e6dc590accfa6b4ec924968a02830
Osmary Ostos - Ceiba Software <osmary.ostos@ceiba.com.co>
2018-05-18 14:31:41 -0500

    pruebas

--- src/test/java/com/parqueadero2/gradle3/ParqueaderoTest.java
+++ src/test/java/com/parqueadero2/gradle3/ParqueaderoTest.java
@@ -5,7 +5,12 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ParqueaderoTest {
 
 	@Test
